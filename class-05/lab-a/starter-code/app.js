de189b903b8937a714d78fe5690ad990e8588479
Jacob <jamsbury95@hotmail.com>
2021-10-09 15:24:28 -0700

    added questionthree

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -52,11 +52,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
-}
+  let sumOne = sum(a, b)[0];
+  let sumTwo = sum(sumOne, c)[0]
+  //console.log (sumTwo);
+  let multiplyOne = multiply(a, b)[0];
+  let multiplyTwo = multiply (multiplyOne, c)[0];
+  //console.log (multiplyTwo)
+  let messageone = `${a} and ${b} and ${c} sum to ${sumTwo}.`;
+  let messagetwo = `The product of ${a} and ${b} and ${c} is ${multiplyTwo}.`;
+  return [sumTwo,multiplyTwo,messageone,messagetwo];
+ }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
