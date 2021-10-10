0fa7c713aa2541f2b090e7a678465f5f30d21258
David W Stokes <fieldmarshalvonkriegenfaust@comcast.net>
2021-10-10 19:26:07 -0400

    fifth commit

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -105,11 +105,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let productFortyOne= multiply(multArr[0],multArr[1])[0];
+  let productFortyTwo=multiply(productFortyOne,multArr[2])[0];
+  let stringExact=`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${productFortyTwo}.`;
+  return [productFortyTwo,stringExact];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
