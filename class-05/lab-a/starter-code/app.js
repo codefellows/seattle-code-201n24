525440bc56ffad66900bb96f4cf1f03b55451978
David W Stokes <fieldmarshalvonkriegenfaust@comcast.net>
2021-10-10 17:44:10 -0400

    third commit

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -35,7 +35,7 @@ let stringMessage=`The product of ${a} and ${b} is ${product}.`;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +52,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+let sumFortyOne= sum(a,b)[0];
+let sumFortyTwo=sum(sumFortyOne,c)[0];
+let productOne=multiply(a,b)[0];
+let productTwo=multiply(productOne,c)[0];
+let stringOne=`${a} and ${b} and ${c} sum to ${sumFortyTwo}.`;
+let stringTwo=`The product of ${a} and ${b} and ${c} is ${productTwo}.`;
+  return [sumFortyTwo,productTwo,stringOne,stringTwo];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
